43bf4b77bedede796a6953bdc2041c08cf75c62b
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2021-09-10 07:12:21 +0100

    Removed debug stuff and improved some code.

--- Main.lua
+++ Main.lua
@@ -31,7 +31,7 @@ if (not NEON) then
 end;
 
 local quick = NEON:github("belkworks", "quick");
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/LegoHacks/Utilities/main/UI.lua"))(); -- Too lazy to use NEON to load the library
+local library = NEON:github("LegoHacks", "Utilities", "UI.lua");
 
 -- Auto updater
 local client = quick.Service.Players.LocalPlayer;
@@ -341,7 +341,6 @@ local roBeats = library:CreateWindow("RoBeats"); do
                     end;
 
                     getupvalue(songDatabase.add_key_to_data, 1):add(song, storedSongs[title]);
-                    storedSongs[title].__key = song;
                     return oldPressed(self, song);
                 end;
 
